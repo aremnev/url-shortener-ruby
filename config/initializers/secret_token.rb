542a7abf12c2edd4a9911be93ecdced72eae051c
Sergey Krivohatskiy <cthutq66a@yandex.ru>
2013-09-24 11:19:19 +0700

    Empty project created. Warning, empty directories with .gitkeep!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UrlShortenerRuby::Application.config.secret_token = '021a146b4bbcc6039b3ab3cb54709e1ac741a35bb7f91e1a9becb9c801268fe8bcc5b2102068b3f76d90dc15795ffd60215d452472e7360ae2b732189154e172'
