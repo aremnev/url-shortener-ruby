e0966521fb7430ca5dc47024fb9179429084e70b
Sergey Krivohatskiy <cthutq66a@yandex.ru>
2013-10-01 13:20:51 +0700

    Some not ready code added in sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,14 @@
 require 'google/api_client'
 
 class SessionsController < ApplicationController
+
   def sign_in
+    session[:user_id] = 1;
     redirect_to :root
   end
 
   def sign_out
+    session[:user_id] = nil;
     redirect_to :root
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 UrlShortenerRuby::Application.routes.draw do
 
-  match '/signin', to: 'Sessions#sign_in'
-  match '/signout', to: 'Sessions#sign_out'
+  match '/signin', to: 'Sessions#sign_in', via: [:get, :post], as: :sign_in
+  match '/signout', to: 'Sessions#sign_out', via: [:delete], as: :sign_out
   get '/', to: 'MainPage#index', as: :root
   match '/shorten', to: 'UrlShortener#shorten', via: [:get, :post], as: :shorten
   get '/:shortenUrl', to: 'UrlShortener#url'
