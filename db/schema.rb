c4e698d8c91e694849c2b5145f2ceaeb39cfb359
Sergey Krivohatskiy <cthutq66a@yandex.ru>
2013-10-01 11:21:39 +0700

    User model added.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130928133023) do
+ActiveRecord::Schema.define(:version => 20131001041938) do
 
   create_table "shorted_urls", :force => true do |t|
-    t.text   "url"
+    t.text     "url"
     t.integer  "follows"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "family_name"
+    t.string   "email"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
 end
