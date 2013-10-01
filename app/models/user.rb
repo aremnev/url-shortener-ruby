2896e6e32d59b221986726875499eebaa000fa52
Sergey Krivohatskiy <cthutq66a@yandex.ru>
2013-10-01 11:46:53 +0700

    Some unit tests added for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :family_name, :name
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, :presence => true, :format => { with: VALID_EMAIL_REGEX }
-  validates :name, :presence => true
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :name, presence: true
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Should create correct user' do
+    user = User.new(:email => 'valid@email.com', :name => 'Lincoln')
+    assert(user.save, 'save must be success')
+    assert(user.valid?, 'user must be valid')
+  end
+  test 'Should not create user without name' do
+    user = User.new(:email => 'otherValid@email.com')
+    assert(!user.save, 'save must not be success without name')
+    assert(!user.valid?, 'user must not be valid without name')
+  end
+  test 'Should not create user without email' do
+    user = User.new(:name => 'Lincoln')
+    assert(!user.save, 'save must not be success without email')
+    assert(!user.valid?, 'user must not be valid without email')
+  end
+  test 'Should not create user with incorrect email' do
+    user = User.new(:email => 'invalid@email,com', :name => 'Lincoln')
+    assert(!user.save, 'save must not be success with incorrect email')
+    assert(!user.valid?, 'user must not be valid with incorrect email')
+  end
 end
