f88cdfc89540a2c14df12d01e70163eb8c52bb9e
Sergey Krivohatskiy <cthutq66a@yandex.ru>
2013-10-03 16:13:00 +0700

    api routes changed to /api/*

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 UrlShortenerRuby::Application.routes.draw do
 
-  match '/signin', to: 'Sessions#sign_in', via: [:get, :post], as: :sign_in
-  match '/signout', to: 'Sessions#sign_out', via: [:delete], as: :sign_out
+  match '/api/signin', to: 'Sessions#sign_in', via: [:get, :post], as: :sign_in
+  match '/api/signout', to: 'Sessions#sign_out', via: [:delete], as: :sign_out
   get '/', to: 'MainPage#index', as: :root
-  match '/shorten', to: 'UrlShortener#shorten', via: [:get, :post], as: :shorten
-  match '/shorten', to: 'UrlShortener#delete', via: [:delete], as: :shorten
+  match '/api/shorten', to: 'UrlShortener#shorten', via: [:get, :post], as: :shorten
+  match '/api/shorten', to: 'UrlShortener#delete', via: [:delete], as: :shorten
 
   get '/:shortenUrl', to: 'UrlShortener#url'
 
