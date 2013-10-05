c7309d63210cf4aff6a8b6275f25ed9cc4f6c7c3
Sergey Krivohatskiy <cthutq66a@yandex.ru>
2013-10-05 17:26:24 +0700

    Shorted urls order added.

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -2,12 +2,15 @@ class MainPageController < ApplicationController
   def index
     if !session[:user_id].nil?
       @user = User.find(session[:user_id])
-      @user_shorted_urls = @user.shorted_urls
+      @user_shorted_urls = @user.shorted_urls.order('created_at')
     else
       @user_shorted_urls = Array.new
       if !session[:shorted_urls].nil?
         session[:shorted_urls].each{ |id|
           @user_shorted_urls.push(ShortedUrl.find(id))
+        }
+        @user_shorted_urls.sort! { |x, y|
+          x.created_at <=> y.created_at
         }
       end
     end
